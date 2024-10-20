769954a5c7abb52899320be10862b66de8d5f8f2
Harley Davidson <harley.dvdson@gmail.com>
2024-10-20 10:37:32 +0500

    test for routes

--- app.ts
+++ app.ts
@@ -1,9 +1,31 @@
-import bcrypt from 'bcrypt';
+import express, { Request, Response } from 'express';
 
-const saltRounds = 13; // Adjust this value (typically between 10 and 12 is a good range)
-const myPlaintextPassword = 'My Long Password with spaces and others';
+const app = express()
+const port = 3000
 
-const salt = bcrypt.genSaltSync(saltRounds);
-const hash = bcrypt.hashSync(myPlaintextPassword, salt);
-console.log(salt);
-console.log(hash);
+app.use(express.json());
+
+const fn = function(req, res) {
+    res.send(req.method + req.route.path.replaceAll('/', '_').replaceAll(':', '$')) ;
+}
+
+app.get('/User', fn);
+app.get('/Pet', fn);
+app.get('/dashboard', fn);
+app.get('/Account', fn);
+app.get('/Payment', fn);
+app.get('/User/:userId', fn);
+app.get('/User/:userId/Pet/:petId', fn);
+app.get('/dashboard2', fn);
+app.get('/dashboard3', fn);
+app.get('/dashboard4', fn);
+app.get('/dashboard5', fn);
+
+
+app.get('/', (req, res) => {
+    res.send('🦄🌈✨👋🌎🌍🌏✨🌈🦄')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
