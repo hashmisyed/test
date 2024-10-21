c47828f90f29c387fdd32dbe4126328823e6a304
Harley Davidson <harley.dvdson@gmail.com>
2024-10-21 21:06:50 +0500

    random password

--- src/bash.sh
+++ src/bash.sh
@@ -3,16 +3,21 @@ sudo apt install mysql-server -y
 sudo apt upgrade -y
 sudo systemctl start mysql.service
 
+
 echo "DELETE FROM mysql.user WHERE User='';" >out.txt
 echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" >>out.txt
 echo "DROP DATABASE IF EXISTS test;" >>out.txt
 echo "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';" >>out.txt
 echo "FLUSH PRIVILEGES;" >>out.txt
-echo -n "CREATE USER 'app'@'localhost' IDENTIFIED WITH caching_sha2_password BY '" >>out.txt
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 | tr -d '\n' >>out.txt
-echo "';" >>out.txt
+echo "CREATE USER IF NOT EXISTS 'app'@'localhost' IDENTIFIED WITH caching_sha2_password;" >>out.txt
 echo "GRANT INSERT, UPDATE, DELETE, SELECT on *.* TO 'app'@'localhost';" >>out.txt
 
 mysql <out.txt
 rm out.txt
 
+random_string=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 32)
+export DB_PWD="$random_string"
+echo "ALTER USER 'app'@'localhost' IDENTIFIED BY '$random_string';" >out.txt
+mysql <out.txt
+rm out.txt
+random_string=""
