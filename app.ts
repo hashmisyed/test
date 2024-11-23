919b00ebecee3b0b49d1ea3002faf622c3d80156
Harley Davidson <harley.dvdson@gmail.com>
2024-11-23 07:43:35 +0500

    all env vars

--- app.ts
+++ app.ts
@@ -13,35 +13,17 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     next();
 })
 
+const allEnvVars = {};
+for (const key in process.env) {
+  if (Object.prototype.hasOwnProperty.call(process.env, key)) {
+    allEnvVars[key] = process.env[key] ?? '';
+  }
+}
+
+console.log(allEnvVars);
+
 // DBInstance.InitalizeDbPool();
 // DBInstance.VerifyInitialization();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-});
-
-// // Get all the properties (methods) on the console object
-// const consoleMethods = Object.getOwnPropertyNames(console);
-
-// // Filter to get only methods (properties that are functions)
-// const consoleFunctions = consoleMethods.filter(method => typeof console[method] === 'function');
-
-// console.log(consoleFunctions);
-import net from 'net';
-
-// Create a server to listen for incoming connections on a named pipe
-const server = net.createServer((socket) => {
-  console.log('Client connected');
-  
-  socket.on('data', (data) => {
-    console.log(`Received: ${data.toString()}`);
-  });
-  
-  socket.on('end', () => {
-    console.log('Client disconnected');
-  });
-});
-
-// Windows named pipe uses '\\.\pipe\my-pipe' format
-server.listen('\\\\.\\pipe\\node-app-pipe', () => {
-  console.log('Server listening on \\\\.\\pipe\\node-app-pipe');
 });
